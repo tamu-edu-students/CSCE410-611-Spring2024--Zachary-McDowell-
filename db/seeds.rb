03960e0bc205605c90aa94b3d286fa7c22261b9c
Zach <cholmcdowell@gmail.com>
2024-02-20 16:10:46 -0600

    reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,6 @@ more_movies = [
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
-  {:title => 'Nach Libre', :rating => 'PG',
-    :release_date => '2006'}
 ]
 
 more_movies.each do |movie|
